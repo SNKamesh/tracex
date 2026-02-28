498a30590f196b6980fd3f4cc515d1797f8c36ec
SNKamesh <kamunet.2006@gmail.com>
2026-03-01 05:28:15 +0530

    fixed errors

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -7,6 +7,7 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen w-full flex bg-transparent text-inherit">
       <Sidebar />
-<main className="flex-1 p-10">{children}</main>
+      <main className="flex-1 p-10">{children}</main>
     </div>
-  );}
+  );
+}
