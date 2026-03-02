5c6d5c28828dcbced60482572932e41b81a7730f
SNKamesh <kamunet.2006@gmail.com>
2026-03-02 15:15:54 +0530

    fix: home link points to /home

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ export default function Sidebar() {
   return (
     <aside className="w-60 bg-slate-900 border-r border-slate-700 p-6">
       <nav className="flex flex-col gap-4">
-        <a href="/">Home</a>
+        <a href="/home">Home</a>
         <a href="/dashboard">Dashboard</a>
         <a href="/study">Study Sessions</a>
         <a href="/plans">Study Plans</a>
