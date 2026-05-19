de166b22122231f763eaba25c09dee4c67f38a3a
SNKamesh <kamunet.2006@gmail.com>
2026-05-19 14:05:48 +0530

    fixed pdf compressions

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -2,11 +2,15 @@
 
 import React from "react";
 
-interface SelectProps
-  extends React.SelectHTMLAttributes<HTMLSelectElement> {
+interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
 export default function Select(props: SelectProps) {
-  return <select {...props} />;
+  return (
+    <select
+      {...props}
+      className={`w-full rounded-lg border border-slate-700 bg-slate-800 px-3 py-2 text-sm text-slate-200 outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 cursor-pointer ${props.className ?? ""}`}
+    />
+  );
 }
